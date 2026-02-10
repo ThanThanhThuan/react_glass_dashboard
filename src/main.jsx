c3caa1d020cadd5b191137b713208103896dde73
ThanThanhThuan <sangxuan1406@gmail.com>
2026-02-10 20:02:46 +0700

    m

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css' // Keep default vite css for resets or clear it
 import './assets/css/glass-style.css' // <--- Import Template CSS
 import { ThemeProvider } from './context/ThemeContext'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
